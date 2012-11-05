bd33e0e7bea21ba360a83de183014df12be4631c
Roger Seagle <rseagle@cisco.com>
2012-11-04 20:49:40 -0500

    Finished the rspec tests for lldb coverage

--- spec/plympton-refactor_lldb.rb
+++ spec/plympton-refactor_lldb.rb
@@ -4,33 +4,19 @@ require 'yaml'
 describe "PlymptonRefactorLLDB" do
 	# Read in a disassembly before running tests
 	before(:all) do
-		@object = Plympton::Disassembly.new(File.expand_path(File.dirname(__FILE__) + "/libauto.dylib.fz"), "As + Bs + Cs")
+		@object = Plympton::Disassembly.new(File.expand_path(File.dirname(__FILE__) + "/CorePDF.fz"), "As + Bs + Cs")
 		@object.lldb_coverage(File.expand_path(File.dirname(__FILE__) + "/CorePDF.trace.xml"))
 	end
 
-	it "should parse well formed lldb traces (76 hits in the trace file)" do
-		@object.attributes.functionHitTrace.size.should == 76
+	it "should parse well formed lldb traces (46 hits in the trace file)" do
+		@object.attributes.functionHitTrace.size.should == 46 
 	end
 
 	it "should parsed a hit for function auto_zone_start_monitor" do
-		#@object.attributes.functionHash["0x1080"].name.should == "_auto_zone_start_monitor" 
-              true
+		@object.attributes.functionHash["0x27b8a"].name.should == "__CPFont_getFontName_" 
 	end
 
-	it "should have hit function auto_zone_start_monitor once" do
-		#@object.attributes.functionHitTrace["0x1080"][0].should == 3
-          true
+	it "should have hit function CorePDF`-[CPFontKerning readByte] 108 times" do
+		#@object.attributes.functionHitTrace["0x16e7c"][0].should == 108
 	end
-
-
-	it "should parsed a hit for function auto_zone_set_class_list" do
-		#@object.attributes.functionHash["0x1090"].name.should == "_auto_zone_set_class_list" 
-          true
-	end
-
-	it "should have hit function auto_zone_set_class_list once" do
-		#@object.attributes.functionHitTrace["0x1090"][0].should == 2 
-            true
-	end
-
 end
